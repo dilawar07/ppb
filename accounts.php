7ce167179e823cdf60175020aa8e615648a9b72e
Dilawar <dilawarsngh@gmail.com>
2020-01-13 14:59:30 -0600

    accounts page created

---
+++ accounts.php
@@ -0,0 +1,51 @@
+<div class="w3-container w3-padding-large" style="margin-bottom:32px" id="accounts">
+  <!-- Accounts Tables -->
+  <h1 class="w3-xxlarge w3-text-red"><b>Accounts.</b></h1>
+  <div class="w3-row-padding" style="margin:0 -16px">
+    <div class="w3-third w3-margin-bottom">
+      <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
+        <li class="w3-red w3-xlarge w3-padding-32">Chequing</li>
+        <li class="w3-padding-16">Free Internet Banking</li>
+        <li class="w3-padding-16">500$ Credit Card</li>
+        <li class="w3-padding-16">1.5% Interest</li>
+        <li class="w3-padding-16">Mail Support</li>
+        <li class="w3-padding-16">
+          <h2>Free</h2>
+          <span class="w3-opacity">No monthly fee</span>
+        </li>
+      </ul>
+    </div>
+
+    <div class="w3-third w3-margin-bottom">
+      <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
+        <li class="w3-red w3-xlarge w3-padding-32">Savings</li>
+        <li class="w3-padding-16">No Service Fee for Cash Withdrawls</li>
+        <li class="w3-padding-16">2000$ Credit Card</li>
+        <li class="w3-padding-16">4.5% Savings Interest</li>
+        <li class="w3-padding-16">Call Support</li>
+        <li class="w3-padding-16">
+          <h2>$ 10</h2>
+          <span class="w3-opacity">per month</span>
+        </li>
+      </ul>
+    </div>
+
+    <div class="w3-third">
+      <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
+        <li class="w3-red w3-xlarge w3-padding-32">Business</li>
+        <li class="w3-padding-16">Insurance Included</li>
+        <li class="w3-padding-16">5000$ Credit Card</li>
+        <li class="w3-padding-16">Loans from 5% Interest</li>
+        <li class="w3-padding-16">Endless Support</li>
+        <li class="w3-padding-16">
+          <h2>$ 30</h2>
+          <span class="w3-opacity">per month</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="w3-container w3-center">
+    Chosen what type of account works for you?<br>
+    <button class="w3-button w3-red w3-padding-large w3-hover-black w3-margin-top">Sign Up Now!</button>
+  </div>
+</div>
