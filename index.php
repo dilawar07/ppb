f2b35930c7228b405eb064736bb289d272f31574
Dilawar <dilawarsngh@gmail.com>
2020-01-13 11:31:30 -0600

    minor tweaks to index

--- index.php
+++ index.php
@@ -42,18 +42,17 @@ body {font-size:16px;}
 <div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
 
 <!-- !PAGE CONTENT! -->
-<div class="w3-main" style="margin-left:300px">
+<div class="w3-main" style="margin-left:300px;">
   <header id="home">
-    <a href="#"><img src="/img/logo.gif" style="width:40px;" class="w3-right w3-margin w3-hide-large w3-hover-opacity"></a>
-    <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
-    <div class="w3-container">
-    <h1 class="w3-jumbo">PridePoint Bank</h1>
+    <div class="w3-container w3-top w3-red">
+      <h6 class="w3-text-white w3-right w3-xxlarge">PridePoint</h6>
+      <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
     </div>
-    <div class="w3-row-padding w3-padding-16">
-      <div class="w3-col m6">
+    <div class="w3-row-padding w3-padding-64">
+      <div class="w3-col m6 w3-padding-16">
         <img src="/img/rrc.jpg" alt="Me" style="width:100%">
       </div>
-      <div class="w3-col m6">
+      <div class="w3-col m6 w3-padding-16">
         <img src="/img/edc.jpg" alt="Me" style="width:100%">
       </div>
     </div>
@@ -68,16 +67,16 @@ body {font-size:16px;}
     <h4>Customer Survey Results</h4>
     <!-- Facts -->
     <p>Customer Satisfaction</p>
-    <div class="w3-grey">
-      <div class="w3-container w3-dark-grey w3-padding w3-center" style="width:95%">95%</div>
+    <div class="w3-pale-red">
+      <div class="w3-container w3-red w3-padding w3-center" style="width:95%">95%</div>
     </div>
     <p>Best Interest Rates</p>
-    <div class="w3-grey">
-      <div class="w3-container w3-dark-grey w3-padding w3-center" style="width:85%">85%</div>
+    <div class="w3-pale-red">
+      <div class="w3-container w3-red w3-padding w3-center" style="width:85%">85%</div>
     </div>
     <p>Easy Loans</p>
-    <div class="w3-grey">
-      <div class="w3-container w3-dark-grey w3-padding w3-center" style="width:80%">80%</div>
+    <div class="w3-pale-red">
+      <div class="w3-container w3-red w3-padding w3-center" style="width:80%">80%</div>
     </div>
     <hr>
   </div>
@@ -88,41 +87,41 @@ body {font-size:16px;}
     <div class="w3-row-padding" style="margin:0 -16px">
       <div class="w3-third w3-margin-bottom">
         <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
-          <li class="w3-black w3-xlarge w3-padding-32">Basic</li>
+          <li class="w3-red w3-xlarge w3-padding-32">Basic</li>
           <li class="w3-padding-16">Web Design</li>
           <li class="w3-padding-16">Photography</li>
           <li class="w3-padding-16">1GB Storage</li>
           <li class="w3-padding-16">Mail Support</li>
           <li class="w3-padding-16">
-            <h2>$ 10</h2>
+            <h2>$ 5</h2>
             <span class="w3-opacity">per month</span>
           </li>
           <li class="w3-light-grey w3-padding-24">
-            <button class="w3-button w3-teal w3-padding-large w3-hover-black">Sign Up</button>
+            <button class="w3-button w3-red w3-padding-large w3-hover-black">Sign Up</button>
           </li>
         </ul>
       </div>
 
       <div class="w3-third w3-margin-bottom">
         <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
-          <li class="w3-teal w3-xlarge w3-padding-32">Pro</li>
+          <li class="w3-red w3-xlarge w3-padding-32">Pro</li>
           <li class="w3-padding-16">Web Design</li>
           <li class="w3-padding-16">Photography</li>
           <li class="w3-padding-16">50GB Storage</li>
           <li class="w3-padding-16">Endless Support</li>
           <li class="w3-padding-16">
-            <h2>$ 25</h2>
+            <h2>$ 15</h2>
             <span class="w3-opacity">per month</span>
           </li>
           <li class="w3-light-grey w3-padding-24">
-            <button class="w3-button w3-teal w3-padding-large w3-hover-black">Sign Up</button>
+            <button class="w3-button w3-red w3-padding-large w3-hover-black">Sign Up</button>
           </li>
         </ul>
       </div>
 
       <div class="w3-third">
         <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
-          <li class="w3-black w3-xlarge w3-padding-32">Premium</li>
+          <li class="w3-red w3-xlarge w3-padding-32">Premium</li>
           <li class="w3-padding-16">Web Design</li>
           <li class="w3-padding-16">Photography</li>
           <li class="w3-padding-16">Unlimited Storage</li>
@@ -132,7 +131,7 @@ body {font-size:16px;}
             <span class="w3-opacity">per month</span>
           </li>
           <li class="w3-light-grey w3-padding-24">
-            <button class="w3-button w3-teal w3-padding-large w3-hover-black">Sign Up</button>
+            <button class="w3-button w3-red w3-padding-large w3-hover-black">Sign Up</button>
           </li>
         </ul>
       </div>
@@ -143,15 +142,15 @@ body {font-size:16px;}
   <div class="w3-container w3-padding-large" id="contact">
     <h1 class="w3-xxlarge w3-text-red"><b>Contact.</b></h1>
     <div class="w3-row-padding w3-center w3-padding-24" style="margin:0 -16px">
-      <div class="w3-third w3-dark-grey">
+      <div class="w3-third w3-red w3-border">
         <p><i class="fa fa-envelope w3-xxlarge w3-text-light-grey"></i></p>
         <p>customercare@pridepoint.com</p>
       </div>
-      <div class="w3-third w3-teal">
+      <div class="w3-third w3-red w3-border">
         <p><i class="fa fa-map-marker w3-xxlarge w3-text-light-grey"></i></p>
         <p>302 Some Street, Winnipeg, CA</p>
       </div>
-      <div class="w3-third w3-dark-grey">
+      <div class="w3-third w3-red w3-border">
         <p><i class="fa fa-phone w3-xxlarge w3-text-light-grey"></i></p>
         <p>+1 431-123-4567</p>
       </div>
@@ -170,7 +169,7 @@ body {font-size:16px;}
         <label>Message</label>
         <textarea class="w3-input w3-border" style="height:100px;resize:none" type="text" name="Message" required> </textarea>
       </div>
-      <button type="submit" class="w3-button w3-black w3-margin-bottom"><i class="fa fa-paper-plane w3-margin-right"></i>Send Message</button>
+      <button type="submit" class="w3-button w3-red w3-margin-bottom"><i class="fa fa-paper-plane w3-margin-right"></i>Send Message</button>
     </form>
   </div>
 
