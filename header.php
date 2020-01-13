37cf4c74c8b8913c204084157eb3e50b1afb4d7d
Dilawar <dilawarsngh@gmail.com>
2020-01-13 16:07:53 -0600

    adds favicon to the site

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <title>PridePoint Bank - Welcome</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" href="images/logoicon.ico"/>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
