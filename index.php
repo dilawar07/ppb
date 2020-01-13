0fa94d467fb5bf0f4d9129c049c67c980ec250da
Dilawar <dilawarsngh@gmail.com>
2020-01-13 12:03:08 -0600

    cosmetic changes

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ body {font-size:16px;}
 <!-- Sidebar/menu -->
 <nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
   <div class="w3-container">
-    <a href="#" onclick="w3_close()" class="w3-hide-large w3-right w3-jumbo w3-padding w3-hover-grey" title="close menu">
+    <a href="#" onclick="w3_close()" class="w3-hide-large w3-right w3-jumbo w3-hover-red" title="close menu">
       <i class="fa fa-remove"></i>
     </a>
     <img src="/img/logo.gif" style="width:35%;" class="w3-round"><br><br>
@@ -23,10 +23,10 @@ body {font-size:16px;}
     <p class="w3-text-grey">the bank you deserve</p>
   </div>
   <div class="w3-bar-block">
-    <a href="#home" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-home fa-fw w3-margin-right"></i>HOME</a>
-    <a href="#about" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-user fa-fw w3-margin-right"></i>ABOUT</a>
-    <a href="#accounts" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-th-large fa-fw w3-margin-right"></i>ACCOUNTS</a>
-    <a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-envelope fa-fw w3-margin-right"></i>CONTACT</a>
+    <a href="#home" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-hover-red"><i class="fa fa-home fa-fw w3-margin-right"></i>HOME</a>
+    <a href="#about" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-hover-red"><i class="fa fa-user fa-fw w3-margin-right"></i>ABOUT</a>
+    <a href="#accounts" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-hover-red"><i class="fa fa-th-large fa-fw w3-margin-right"></i>ACCOUNTS</a>
+    <a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-hover-red"><i class="fa fa-envelope fa-fw w3-margin-right"></i>CONTACT</a>
   </div>
   <div class="w3-panel w3-large">
     <i class="fa fa-facebook-official w3-hover-opacity"></i>
@@ -44,16 +44,13 @@ body {font-size:16px;}
 <!-- !PAGE CONTENT! -->
 <div class="w3-main" style="margin-left:300px;">
   <header id="home">
-    <div class="w3-container w3-top w3-red">
-      <h6 class="w3-text-white w3-right w3-xxlarge">PridePoint</h6>
-      <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
-    </div>
-    <div class="w3-row-padding w3-padding-64">
-      <div class="w3-col m6 w3-padding-16">
-        <img src="/img/rrc.jpg" alt="Me" style="width:100%">
+    <span class="w3-button w3-hide-large w3-xxlarge w3-hover-pale-red w3-top w3-red" onclick="w3_open()"><i class="fa fa-bars w3-left"> PridePoint</i></span>
+    <div class="w3-row-padding">
+      <div class="w3-col m6 w3-padding" style="margin-top:50px">
+        <img src="/img/rrc.jpg" alt="Bank" style="width:100%">
       </div>
-      <div class="w3-col m6 w3-padding-16">
-        <img src="/img/edc.jpg" alt="Me" style="width:100%">
+      <div class="w3-col m6 w3-padding" style="margin-top:50px">
+        <img src="/img/edc.jpg" alt="RRC EDC" style="width:100%">
       </div>
     </div>
   </header>
@@ -87,10 +84,10 @@ body {font-size:16px;}
     <div class="w3-row-padding" style="margin:0 -16px">
       <div class="w3-third w3-margin-bottom">
         <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
-          <li class="w3-red w3-xlarge w3-padding-32">Basic</li>
+          <li class="w3-red w3-xlarge w3-padding-32">Chequing</li>
           <li class="w3-padding-16">Web Design</li>
-          <li class="w3-padding-16">Photography</li>
-          <li class="w3-padding-16">1GB Storage</li>
+          <li class="w3-padding-16">500$ Credit Card</li>
+          <li class="w3-padding-16">1.5% Interest</li>
           <li class="w3-padding-16">Mail Support</li>
           <li class="w3-padding-16">
             <h2>$ 5</h2>
@@ -104,11 +101,11 @@ body {font-size:16px;}
 
       <div class="w3-third w3-margin-bottom">
         <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
-          <li class="w3-red w3-xlarge w3-padding-32">Pro</li>
+          <li class="w3-red w3-xlarge w3-padding-32">Savings</li>
           <li class="w3-padding-16">Web Design</li>
-          <li class="w3-padding-16">Photography</li>
-          <li class="w3-padding-16">50GB Storage</li>
-          <li class="w3-padding-16">Endless Support</li>
+          <li class="w3-padding-16">2000$ Credit Card</li>
+          <li class="w3-padding-16">4.5% Savings Interest</li>
+          <li class="w3-padding-16">Call Support</li>
           <li class="w3-padding-16">
             <h2>$ 15</h2>
             <span class="w3-opacity">per month</span>
@@ -121,10 +118,10 @@ body {font-size:16px;}
 
       <div class="w3-third">
         <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
-          <li class="w3-red w3-xlarge w3-padding-32">Premium</li>
+          <li class="w3-red w3-xlarge w3-padding-32">Business</li>
           <li class="w3-padding-16">Web Design</li>
-          <li class="w3-padding-16">Photography</li>
-          <li class="w3-padding-16">Unlimited Storage</li>
+          <li class="w3-padding-16">Business Credit Cards from Employees</li>
+          <li class="w3-padding-16">Loans from 5% Interest</li>
           <li class="w3-padding-16">Endless Support</li>
           <li class="w3-padding-16">
             <h2>$ 25</h2>
@@ -174,45 +171,8 @@ body {font-size:16px;}
   </div>
 
   <!-- Footer -->
-  <footer class="w3-container w3-padding-32 w3-dark-grey">
-  <div class="w3-row-padding">
-    <div class="w3-third">
-      <h3>FOOTER</h3>
-      <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
-      <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
-    </div>
+  <div class="w3-red w3-center w3-padding-16">PridePoint Bank | All Rights Unreserved | Copywrongs  2019 | Powered by Arby's</div>
 
-    <div class="w3-third">
-      <h3>BLOG POSTS</h3>
-      <ul class="w3-ul w3-hoverable">
-        <li class="w3-padding-16">
-          <img src="/w3images/workshop.jpg" class="w3-left w3-margin-right" style="width:50px">
-          <span class="w3-large">Lorem</span><br>
-          <span>Sed mattis nunc</span>
-        </li>
-        <li class="w3-padding-16">
-          <img src="/w3images/gondol.jpg" class="w3-left w3-margin-right" style="width:50px">
-          <span class="w3-large">Ipsum</span><br>
-          <span>Praes tinci sed</span>
-        </li>
-      </ul>
-    </div>
-
-    <div class="w3-third">
-      <h3>POPULAR TAGS</h3>
-      <p>
-        <span class="w3-tag w3-black w3-margin-bottom">Travel</span> <span class="w3-tag w3-grey w3-small w3-margin-bottom">New York</span> <span class="w3-tag w3-grey w3-small w3-margin-bottom">London</span>
-        <span class="w3-tag w3-grey w3-small w3-margin-bottom">IKEA</span> <span class="w3-tag w3-grey w3-small w3-margin-bottom">NORWAY</span> <span class="w3-tag w3-grey w3-small w3-margin-bottom">DIY</span>
-        <span class="w3-tag w3-grey w3-small w3-margin-bottom">Ideas</span> <span class="w3-tag w3-grey w3-small w3-margin-bottom">Baby</span> <span class="w3-tag w3-grey w3-small w3-margin-bottom">Family</span>
-        <span class="w3-tag w3-grey w3-small w3-margin-bottom">News</span> <span class="w3-tag w3-grey w3-small w3-margin-bottom">Clothing</span> <span class="w3-tag w3-grey w3-small w3-margin-bottom">Shopping</span>
-        <span class="w3-tag w3-grey w3-small w3-margin-bottom">Sports</span> <span class="w3-tag w3-grey w3-small w3-margin-bottom">Games</span>
-      </p>
-    </div>
-
-  </div>
-  </footer>
-
-  <div class="w3-black w3-center w3-padding-24">Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-opacity">w3.css</a></div>
 
 <!-- End page content -->
 </div>
