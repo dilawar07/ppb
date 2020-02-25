4de7aeb0339b73dfa60adc40f1f726ea97e7e12a
Dilawar <dilawarsngh@gmail.com>
2020-02-25 11:02:39 -0600

    remove animation from sidebar

--- nav.php
+++ nav.php
@@ -1,6 +1,6 @@
 <div style="float:top:0px; ">
 <!-- Sidebar/menu -->
-<nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;top:0px;" id="mySidebar"><br>
+<nav class="w3-sidebar w3-collapse w3-white" style="z-index:3;width:300px;top:0px;" id="mySidebar"><br>
   <div class="w3-container">
     <a href="#" onclick="w3_close()" class="w3-hide-large w3-right w3-jumbo w3-hover-red" title="close menu">
       <i class="fa fa-remove"></i>
