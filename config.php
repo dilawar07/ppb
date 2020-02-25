73d8d322a0fe5cfcae8c9811f0e9798b637bda2e
Dilawar <dilawarsngh@gmail.com>
2020-02-25 11:01:11 -0600

    changes to config file

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
   define('DEBUG',true);
-  
+
   if (!DEBUG) {
     error_reporting(0);
     ini_set('display_errors', 0);
@@ -9,22 +9,20 @@
   } else {
     error_reporting(E_ALL);
   }
-  
+
   define('ADMIN_ADDRESS','blog_admin@mailinator.com');
 
   define('DB_HOSTNAME', 'localhost');
-  //define('DB_USER',     'adiwal');
   define('DB_PASSWORD', 'adiwal@1.1');
   define('DB_DATABASE', 'pp_bank');
-  
-  define('BLOG_NAME','Stung Eye');
-  define('BLOG_INDEX_NUM_POSTS',5);
-  
+
+  define('BLOG_NAME','Pridepoint Bank');
+
   function format_mysql_datetime($datetime) {
     $time = strtotime($datetime);
     return date('F j, Y, g:i a', $time);
   }
-  
+
   function redirect($script_name = 'index.php') {
     header("Location: $script_name");
     exit;
