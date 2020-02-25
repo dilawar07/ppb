cae60932644b97937dcabb3541392223fc9a099b
Dilawar <dilawarsngh@gmail.com>
2020-02-25 11:01:35 -0600

    tweaks logout.php

--- logout.php
+++ logout.php
@@ -1,23 +1,4 @@
 <?php
-  require 'config.php';
-  require 'database.php';
-  $g_title = BLOG_NAME . ' - logout';
-  $g_page = 'logout';
- require 'header.php';
-  require 'nav.php';
-//require 'banner.php';
-?>
-
-
-<div id="all_blogs">
-<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
-<tr>
-
-
-<?php 
-// Put this code in first line of web page. 
-//session_start();
-
 session_destroy();
 $message = 'Logged Out Successfully!!';
 echo "<script type='text/javascript'>
@@ -25,11 +6,4 @@ echo "<script type='text/javascript'>
 	</script>";
 header("Refresh:0; url=index.php", true, 303);
 
-?>
-
-</tr>
-</table>
-</div>
-<?php
-  require 'footer.php';
 ?>
