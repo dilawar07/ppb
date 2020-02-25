01350ef1c9a51ce488cab13f76c1d58da082153a
Dilawar <dilawarsngh@gmail.com>
2020-02-25 14:23:25 -0600

    database updated

--- databaseconnection.php
+++ databaseconnection.php
@@ -1,18 +1,16 @@
 <?php
 
+//Define variables needed to connect to the MySQL database
+define('DB_DSN', 'mysql:host=localhost;dbname=pp_bank;charset=utf8');
+define('DB_USER', 'admin');
+define('DB_PASS', 'iamtheadmin123');
 
-
-    //Define variables needed to connect to the MySQL database
-    define('DB_DSN', 'mysql:host=localhost;dbname=pp_bank;charset=utf8');
- define('DB_USER', 'adiwal');
-   define('DB_PASS', 'adiwal@1.1');
-
-    //Connect to the database. If the connection fails the main form will not display
-    try {
-        $db = new PDO(DB_DSN, DB_USER, DB_PASS);
-    } catch (PDOException $e) {
-        echo 'Error: '.$e->getMessage();
-        die(); // Force execution to stop on errors.
-    }
+//Connect to the database. If the connection fails the main form will not display
+try {
+	$db = new PDO(DB_DSN, DB_USER, DB_PASS);
+} catch (PDOException $e) {
+	echo 'Error: '.$e->getMessage();
+	die(); // Force execution to stop on errors.
+}
 ?>
 
